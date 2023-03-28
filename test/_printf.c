84c2994e0f668ac565cfaf073a7c75bf166713bc
nimrod02 <letrun.a@gmail.com>
2023-03-28 16:28:18 +0200

    [ADD] _printf file with putchar in it

--- test/_printf.c
+++ test/_printf.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
-
 int _printf(const char *format, ...)
 {
-	int get_func;
-	va_list args;
+        int index = 0;
 
-	datatype callfunc[] = { {'c', print_char},
-				{'s', print_str},
-						};
-	va_start (args, format);
-	if (format != NULL)
-		return (EXIT_FAILURE);
-	get_func = printer(args, callfunc, get_func);
-	va_end(args);
-	return(get_func);
+        if (format != NULL)
+                return (EXIT_FAILURE);
+        for (; index != '\0'; index++)
+                _putchar (format[index]);
+return (EXIT_SUCCESS);
 }
