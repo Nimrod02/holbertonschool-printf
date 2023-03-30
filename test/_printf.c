3807cde7eebaa7ac286da9be67e404a7a4c14aa0
nimrod02 <letrun.a@gmail.com>
2023-03-30 10:33:52 +0200

    [UPDATE] update _printf.c, add new loop and condition

--- test/_printf.c
+++ test/_printf.c
@@ -3,10 +3,24 @@
 int _printf(const char *format, ...)
 {
         int index = 0;
+	int (*f)
+	va_list list;
+	va_start (list, format);
 
-        if (format != NULL)
+        if (format == NULL)
                 return (EXIT_FAILURE);
-        for (; index != '\0'; index++)
-                _putchar (format[index]);
-return (EXIT_SUCCESS);
+
+	while (format[index])
+	{
+		if (format[index] != '%')
+			_putchar (format[index]);		
+			if (format[index] == '%' && (format[index] != 'c' || format[index] != 's'))
+					_putchar (format[index]);
+			
+		index++;
+	}
+
+	va_end(list);
+
+	return (EXIT_SUCCESS);
 }
