373cac3c87f33b10b54505aa502562297bcdcf76
nimrod02 <letrun.a@gmail.com>
2023-03-31 22:04:03 +0200

    Update get_function with betty syntax

--- get_function.c
+++ get_function.c
@@ -3,6 +3,9 @@
 /**
  * print_s - fonction print a string
  * @str: char pointer to form a string
+ * @list: list of argument
+ * 
+ * Return: return the index
 */
 
 int print_s(va_list list)
@@ -23,12 +26,19 @@ int print_s(va_list list)
 /**
  * print_c - fonction print char
  * @character: character
+ * @list: list of arguments
 */
 
 void print_c(va_list list)
 {
 	_putchar (va_arg(list, int));
 }
+
+/**
+ * print_pourcent - fonction print a pourcent
+ * @list: list of arguments
+ * Return: always 1
+*/
 
 int print_pourcent(__attribute((unused))va_list list)
 {
