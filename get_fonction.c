8497ca7dace279d4c579125eabae9c7abbb27f4a
nimrod02 <letrun.a@gmail.com>
2023-03-31 09:58:10 +0200

    [ADD] get_function file, with rewrite function.

--- get_fonction.c
+++ get_fonction.c
@@ -1,27 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
  *print_s - fonction print a string
  *@str: sting 
 */
 
-    void print_s(char *str)
-    {
-        int index = 0;
+void print_s(va_list list)
+{
+    char *str;
+    int index = 0;
+    
+    str = va_arg (list, char *);
+
+    if (str == NULL)
+            str = "(null)";
+
         for (; index < '\0'; index++)
             _putchar (str[index]);
-    }
+}
 
 /**
  *print_c - fonction print char 
  *@character: character
 */
 
-    void print_c(char character)
-    {  
-        _putchar (character);
-    }
+void print_c(va_list list)
+{
+    _putchar (va_arg(list, int));
+}
 
     
 
